58e9fb86ce4e44d2051b0666d7146323c6ba4319
psw <swpark@yellotail.com>
2021-10-29 18:13:07 +0900

    first commit

---
+++ client-react/src/App.js
@@ -0,0 +1,34 @@
+import logo from './static/image/logo512.png';
+import './static/css/App.css';
+import './static/css/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+// import sidebar from './components/layout/SideNav'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar pt-3">
+        <FontAwesomeIcon icon={faBars} color="#e5e5e5" size="lg" transform="bottom-4"/>
+      </div>
+
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This <code>src/App.js</code> and save to reload.
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          select button
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
